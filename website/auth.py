448fffc65b1970b502740afe8418dc067981f0cd
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-11-18 11:08:55 +0100

    can make a account

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint,render_template, request, flash
+from flask import Blueprint,render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 auth = Blueprint('auth', __name__)
 
@@ -15,19 +20,23 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error') ## flash message for error. if success use category='success'
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be grater than 1 character', category='error') 
         elif password1 != password2:
             flash('password must be at least 7 characters.', category='error') 
         elif len(password1) < 7:
             flash('password must be at least 7 characters.', category='error') 
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
